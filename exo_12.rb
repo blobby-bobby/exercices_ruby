b99b8002962ed90bbef755cb97cd8ad457a52331
blobby-bobby <bulle.ouvrard@gmail.com>
2023-01-17 13:36:28 +0100

    better comments i hope

--- exo_12.rb
+++ exo_12.rb
@@ -1,15 +1,23 @@
+#Demander année de naissance
 puts "Tu es né en quelle année ?"
 print "> "
+
+#Réponse convertie en nombre
 user_birth_year = gets.chomp.to_i
+
 annee_actuelle = 2023
 user_age = annee_actuelle - user_birth_year
+
+#L'âge de l'utilisateur à l'année user_age
 i = 1
+
 puts "Il y a #{user_age} ans, tu n'avais même pas #{i} an."
 
-
+# On avance dans le temps jusqu'au premier anniv donc on décrémente user_age
 user_age -=1
 puts "Il y a #{user_age} ans, tu avais #{i} an."
 
+# On avance dans le temps d'un an donc on décrémente user_age et on incrémente l'âge qui augmente avec le temps
 user_age -=1
 i +=1
 
